8dc70397b2b750b07732109333cbc2a8a3534999
Uladzimir Kazak <uladzimir_kazak@epam.com>
2023-03-28 22:42:22 +0300

    feat: images rm

--- src/common/products.mock.ts
+++ src/common/products.mock.ts
@@ -19,19 +19,14 @@ const cats = [
   "Ragdoll",
   "Exotic Shorthair",
   "British Shorthair",
-  "Persian",
   "Maine Coon",
-  "American Shorthair",
   "Devon Rex",
   "Sphynx",
   "Scottish Fold",
   "Abyssinian",
-  "Oriental",
   "Siamese",
   "Cornish Rex",
-  "Bengal",
   "Russian Blue",
-  "Siberian",
 ];
 
 function getRandomInt(min, max) {
@@ -40,21 +35,13 @@ function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
 }
 
-const client_id =
-  "87e26779aa6242a2b2fc8e863886185d1d1f07215e4890071e45448baedf8950";
-
 export const mockProducts: Promise<Product[]> = Promise.all(
   cats.map(async (cat, i) => {
-    const res = await axios.get(
-      `https://api.unsplash.com/search/photos/?client_id=${client_id}&query=${cat}`
-    );
-    console.log(res);
     return {
       title: cat,
       description: `description ${i}`,
       id: `${i}-id`,
       price: getRandomInt(1, 50),
-      image: res.data.results[0].urls.small,
     };
   })
 );
