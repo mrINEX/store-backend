6810b92fca809bd507349fc03ecbd95f56a975e6
Uladzimir Kazak <uladzimir_kazak@epam.com>
2023-04-01 14:45:38 +0300

    feat: rm @middy/http-cors

--- src/libs/lambda.ts
+++ src/libs/lambda.ts
@@ -1,16 +1,12 @@
 import middy from "@middy/core";
 import middyJsonBodyParser from "@middy/http-json-body-parser";
 import errorHandler from "@middy/http-error-handler";
-import cors from "@middy/http-cors";
 // import validator from "@middy/validator";
 // import { transpileSchema } from "@middy/validator/transpile";
 // import { Context } from "aws-lambda";
 
 export const middyfy = (handler) => {
-  return middy(handler)
-    .use(middyJsonBodyParser())
-    .use(errorHandler())
-    .use(cors());
+  return middy(handler).use(middyJsonBodyParser()).use(errorHandler());
 };
 
 // if (schema !== undefined) {
