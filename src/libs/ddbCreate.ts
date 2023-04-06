239b453ac954c0145dcadf6ed864503a9133c802
Uladzimir Kazak <uladzimir_kazak@epam.com>
2023-04-06 21:09:46 +0300

    feat: add create and fill table scripts

--- src/libs/ddbCreate.ts
+++ src/libs/ddbCreate.ts
@@ -1,7 +1,5 @@
 import * as clientDynamodb from "@aws-sdk/client-dynamodb";
-import { PutCommand } from "@aws-sdk/lib-dynamodb";
-import { ddbClient, ddbDocClient } from "./ddbClient";
-import { Product, getProductService } from "./productService";
+import { ddbClient } from "./ddbClient";
 
 const productsParams = {
   AttributeDefinitions: [{ AttributeName: "id", AttributeType: "S" }],
@@ -29,16 +27,6 @@ export const run = async () => {
     const stocksExist = tables.TableNames.includes(stocksParams.TableName);
 
     if (productsExist || stocksExist) {
-      for (const table of tables.TableNames) {
-        console.log("Table", table);
-
-        if (table === productsParams.TableName) {
-          const productService = getProductService();
-          const products = await productService.getProducts();
-
-          await Promise.all(products.map((product) => putItem(table, product)));
-        }
-      }
       return;
     }
 
@@ -50,34 +38,9 @@ export const run = async () => {
     for (const table of data) {
       const { TableArn: arn, TableName } = table.TableDescription;
       console.log(`Table ${TableName} created`, { arn });
-
-      if (TableName === productsParams.TableName) {
-        const productService = getProductService();
-        const products = await productService.getProducts();
-
-        await Promise.all(
-          products.map((product) => putItem(TableName, product))
-        );
-      }
     }
   } catch (err) {
     console.log("Error", err);
   }
 };
 run();
-
-const putItem = async (table: string, item: Product) => {
-  const params = {
-    TableName: table,
-    Item: {
-      primaryKey: item.id,
-      ...item,
-    },
-  };
-  try {
-    const data = await ddbDocClient.send(new PutCommand(params));
-    console.log("Success - item added or updated", data);
-  } catch (err) {
-    console.log("Error", err.stack);
-  }
-};
